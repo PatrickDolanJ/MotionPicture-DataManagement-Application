85b5621302bd9dda9a10e5cf0f47e2f16b9a5bb1
MamaDutchy <87340915+MamaDutchy@users.noreply.github.com>
2022-08-08 23:29:44 -0400

    added sorting

--- FrontEnd/motion-picture-front/src/Components/Table.vue
+++ FrontEnd/motion-picture-front/src/Components/Table.vue
@@ -7,9 +7,9 @@
         <table id="movie-table" class="table table-striped table-dark table-sm">
           <thead class="thead-dark">
             <tr>
-              <th scope="col"><button>Description</button></th>
-              <th scope="col"><button>Description</button></th>
-              <th scope="col"><button>Release Year</button></th>
+              <th scope="col"><button @click="sortResults('Title')" type="button">Title</button></th>
+              <th scope="col"><button @click="sortResults('Description')" type="button">Description</button></th>
+              <th scope="col"><button @click="sortResults('ReleaseYear')" type="button">Release Year</button></th>
               <th scope ="col">Actions</th>
             </tr>
           </thead>
@@ -36,23 +36,53 @@ export default{
   data(){
     return {
       movies: [],
-      isLoading: true,
-      MovieTitleFilter: ""
+      SortTitleAsc: false,
+      SortDescriptionAsc: false,
+      SortYearAsc: false
     }
   },
   created(){
     MovieService.getMovies().then(resposne => {
       console.log("got the response")
       this.movies = resposne.data;
-      this.isLoading = false;
     });
+  },
+
+  methods:{
+
+    sortResults(sorter,asc) {
+      asc = null;
+      
+      if(sorter=='Title'){
+        this.SortTitleAsc = !this.SortTitleAsc;
+        asc=this.SortTitleAsc;
+      } else if (sorter==='Description'){
+        this.SortDescriptionAsc = !this.SortDescriptionAsc;
+        asc = this.SortDescriptionAsc;
+      } else {
+        this.SortYearAsc = !this.SortYearAsc;
+        asc = this.SortYearAsc;
+      }
+     
+
+      this.movies =this.movies.sort(function(a,b){
+        if(asc){
+          return (a[sorter]>b[sorter]) ? 1 : (a[sorter]<b[sorter] ? -1 : 0);
+        } else {
+          return (b[sorter]>a[sorter]) ? 1 : (b[sorter]<a[sorter] ? -1 : 0);
+        }
+      })
+      }
+
+    }
   }
-}
+
 
 </script>
 
 
 <style style="scoped">
+
 .movie-table{
   background-color: white;
   
@@ -83,8 +113,23 @@ table button{
   background: none;
   border: none;
   color: white;
-
 }
+
+.thead-dark button:hover{
+ color:cornflowerblue;
+ font-weight: 600;
+}
+
+.thead-dark {
+font-family: "Avenir", Helvetica, Arial, sans-serif;
+font-size: 20px;
+}
+
+
+.action-buttons button:hover{
+  background-color: cornflowerblue;
+}
+
 
 .delete-button{
   color:crimson;
@@ -98,5 +143,9 @@ table button{
 .action-buttons{
 width:100px;
 align-items: center;
+}
+
+.moviie-release-year{
+  width: 150px;
 }
 </style>
